885e3b9779b84803770a802a052195a59334bd8d
Brendon BOUZAS <brendon.bouzas@ymail.com>
2019-03-17 12:46:35 +0900

    added enzymes packages, and implemented and passed first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme, { mount } from "enzyme";
+Enzyme.configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("rendering components", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe("The root App component ", () => {
+  it("should render the Title of the of the App", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.text()).toContain(
+      "Vote for players to represent your region's team"
+    );
+  });
 });
